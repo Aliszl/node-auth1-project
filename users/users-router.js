ab0aed0e7a9981fe7ad88c3345964cf7e5c31a02
Alison Law <alisparklaw@gmail.com>
2020-03-23 14:07:35 +0000

    logout and protected get success with postman.  MVP

--- users/users-router.js
+++ users/users-router.js
@@ -6,7 +6,7 @@ function protected(req, res, next) {
   if (req.session && req.session.user) {
     next()
   } else {
-    res.status(401).json({ message: 'you have no session here' })
+    res.status(401).json({ message: 'no session' })
   }
 }
 
